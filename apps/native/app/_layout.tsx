ed0e1adfc66ce5a9b3a1c6f2f9cf245ba3bd30b2
mtumae <mtume2016@gmail.com>
2025-12-12 12:09:35 +0300

    initial commit

---
+++ apps/native/app/_layout.tsx
@@ -0,0 +1,46 @@
+import "@/global.css";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+import { Stack } from "expo-router";
+import { HeroUINativeProvider } from "heroui-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+import { AppThemeProvider } from "@/contexts/app-theme-context";
+
+export const unstable_settings = {
+	initialRouteName: "(drawer)",
+};
+
+const convexUrl = process.env.EXPO_PUBLIC_CONVEX_URL || "";
+const convex = new ConvexReactClient(convexUrl, {
+	unsavedChangesWarning: false,
+});
+
+function StackLayout() {
+	return (
+		<Stack screenOptions={{}}>
+			<Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+			<Stack.Screen
+				name="modal"
+				options={{ title: "Modal", presentation: "modal" }}
+			/>
+		</Stack>
+	);
+}
+
+export default function Layout() {
+	return (
+		<ConvexProvider client={convex}>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<KeyboardProvider>
+					<AppThemeProvider>
+						<HeroUINativeProvider>
+							<StackLayout />
+						</HeroUINativeProvider>
+					</AppThemeProvider>
+				</KeyboardProvider>
+			</GestureHandlerRootView>
+		</ConvexProvider>
+	);
+}
